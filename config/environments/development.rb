265742a69b988ea8d9e52ff8ae8ad1bec68f167d
Tiago Luiz Ferreira <tiagolf@gmail.com>
2018-06-29 23:44:14 -0300

    ajuste do ambiente

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Desabilitar mensagens: Cannot render console from.....
+  config.web_console.whiny_requests = false
+
 end
